5aab149a62101048832f5ea6cd6f04a51ee53205
sam <samuelcalderon.dev@gmail.com>
2026-03-22 09:15:26 -0500

    chore: eliminar import Badge no utilizado en InventarioVitrinaTab

--- erp-vitrinas/components/admin/InventarioVitrinaTab.tsx
+++ erp-vitrinas/components/admin/InventarioVitrinaTab.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Badge } from '@/components/ui/badge'
 import { Skeleton } from '@/components/ui/skeleton'
 import { useInventarioVitrina } from '@/lib/hooks/useInventarioVitrina'
 import type { InventarioVitrinaRow } from '@/lib/hooks/useInventarioVitrina'
